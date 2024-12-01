695e0e8e3be1722d9cb21717cd99675cc17b3579
ural360 <75667422+ural360@users.noreply.github.com>
2024-12-01 13:21:37 +0500

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,52 @@
+from random import randint
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.ability = self.get_ability()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+    def get_ability(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities'][0]['ability']['name']) 
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['home']['front_default'])
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}"
+    def infa(self):
+        return f"Способность: {self.ability}"
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+    
+
+
