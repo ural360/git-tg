6036e0403c1c220821d9f4b6ccb7020ed73a1037
ural360 <75667422+ural360@users.noreply.github.com>
2024-12-01 13:20:54 +0500

    упдате

--- bot.py
+++ bot.py
@@ -1,49 +1,19 @@
-import telebot # библиотека telebot
-from config import token # импорт токена
+import telebot 
+from config import token
+
+from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
-class MyClass:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
 
-    def __str__(self):
-        return f"Данные экземпляра: Данные номер 1:{self.value1}, данные номер 2:{self.value2} "
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
-
-@bot.message_handler(commands=['ban'])
-def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
-        user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
-        if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
-        else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
-            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+@bot.message_handler(commands=['go'])
+def go(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_message(message.chat.id, pokemon.infa())
+        bot.send_photo(message.chat.id, pokemon.show_img())
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
-@bot.message_handler(commands=['create'])
-def create_instance(message): 
-        args = message.text.split()[1:]  
-        if len(args) != 2:
-            bot.reply_to(message, "Пожалуйста, введите два значения. Пример: /create значение1 значение2")
-        value1 = args[0]
-        value2 = args[1]
-        instance = MyClass(value1, value2)
-        bot.reply_to(message, f"Экземпляр создан: {instance}")
-@bot.message_handler(content_types=['new_chat_members'])
-def make_some(message):
-    bot.send_message(message.chat.id, 'I accepted a new user!')
-    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+        bot.reply_to(message, "Ты уже создал себе покемона")
+
 
 bot.infinity_polling(none_stop=True)
-
