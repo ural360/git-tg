bf2b8a309f1661b0bfac83994e37cf96c621f16f
ural360 <75667422+ural360@users.noreply.github.com>
2024-12-01 10:40:33 +0500

    update

--- bot.py
+++ bot.py
@@ -2,7 +2,13 @@ import telebot # библиотека telebot
 from config import token # импорт токена
 
 bot = telebot.TeleBot(token) 
+class MyClass:
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
 
+    def __str__(self):
+        return f"Данные экземпляра: Данные номер 1:{self.value1}, данные номер 2:{self.value2} "
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
@@ -22,5 +28,22 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+@bot.message_handler(commands=['create'])
+def create_instance(message): 
+        args = message.text.split()[1:]  
+        if len(args) != 2:
+            bot.reply_to(message, "Пожалуйста, введите два значения. Пример: /create значение1 значение2")
+        value1 = args[0]
+        value2 = args[1]
+        instance = MyClass(value1, value2)
+        bot.reply_to(message, f"Экземпляр создан: {instance}")
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 bot.infinity_polling(none_stop=True)
+
